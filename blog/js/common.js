5bd4422f786be7205ae45a9e255a9e90a390e902
Argevolle <argevolle@outlook.com>
2015-07-03 14:19:54 +0800

    头部完成 85% 内容（响应式未处理）

--- blog/js/common.js
+++ blog/js/common.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+$(document).ready(function() {
+    var owl = $('#owl');
+    owl.owlCarousel({
+        items: 4,
+        autoPlay: true,
+        navigation: true,
+        navigationText: [
+            '<a class="prev"><i class="fa fa-chevron-left"></i></a>',
+            '<a class="next"><i class="fa fa-chevron-right"></i></a>'
+        ],
+        pagination: false
+    });
+});
